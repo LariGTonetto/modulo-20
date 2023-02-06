6fe7a0c13481c0678f157ab497b33ff9c0be6f46
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-02-06 15:54:18 -0300

    Update desafio-1.js

--- desafio-1.js
+++ desafio-1.js
@@ -1,14 +1,11 @@
-export function mdc(num1, num2) {
-
-    var resto;
-
+export function MDC(N1, N2)
+{var resto;
     do {
-        resto = num1 % num2;
-
-        num1 = num2;
-        num2 = resto;
-
-    } while (resto != 0);
-    console.log(num1);
-    return num1;
-}
+        resto = N1 % N2;
+        N1 = N2;
+        N2 = resto;
+        } 
+    while (resto != 0);
+    console.log(N1);
+    return N1;
+    }
