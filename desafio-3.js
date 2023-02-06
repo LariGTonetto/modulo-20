0192df306e63566179afde6e99e1448a4c7147b7
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-02-06 16:11:17 -0300

    Update desafio-3.js

--- desafio-3.js
+++ desafio-3.js
@@ -1,12 +1,11 @@
-let totalDaSoma = 0
-function getTotalDaSoma() {
-for (var i = 0; i < 1000; i++) {
-    if (i % 5 == 0 || i % 7 == 0) {
-        totalDaSoma += i;
-    }
+let Soma = 0
+function getSoma() 
+{
+for (var a = 0; a < 1000; a++) 
+{if (a % 5 == 0 || a % 7 == 0) 
+{Soma += a;}
 }
-
-return totalDaSoma
+return Soma
 }
-console.log(getTotalDaSoma())
-export default totalDaSoma
+console.log(getSoma())
+export default Soma
