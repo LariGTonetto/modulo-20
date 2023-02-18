c5c1664b67b49c25b580bcc60bb7c96ccceceed3
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-02-18 19:50:31 -0300

    Update desafio-3.test.js

--- desafio-3.test.js
+++ desafio-3.test.js
@@ -1,7 +1,7 @@
 import {expect} from '@jest/globals';
 import React from "react";
 import renderer from "react-test-renderer";
-import totalSoma from "../../Mod20/desafio-3";
+import Soma from "../../Mod20/desafio-3";
 
 test('This is validated', () => {
     const tree = renderer.create(<desafio-3 />).toJSON();
@@ -9,5 +9,5 @@ test('This is validated', () => {
 });
 
 test('That result of smaller', () => {
-    expect(totalSoma).toEqual(156361);
+    expect(Soma).toEqual(156361);
 })
