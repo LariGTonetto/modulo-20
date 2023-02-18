37490f61e2007c70ec3849414ee56a4fda01aead
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-02-18 19:49:11 -0300

    Update desafio-3.test.js

--- desafio-3.test.js
+++ desafio-3.test.js
@@ -1,10 +1,10 @@
 import {expect} from '@jest/globals';
 import React from "react";
 import renderer from "react-test-renderer";
-import totalSoma from "../../Mod20/somaInt";
+import totalSoma from "../../Mod20/desafio-3";
 
 test('This is validated', () => {
-    const tree = renderer.create(<somaInt />).toJSON();
+    const tree = renderer.create(<desafio-3 />).toJSON();
     expect(tree).toMatchSnapshot();
 });
 
