21938a91ab57846e5d0fc8f48078392c97283730
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-02-18 19:46:23 -0300

    Update desafio-1.test.js

--- desafio-1.test.js
+++ desafio-1.test.js
@@ -1,14 +1,14 @@
 import {expect} from '@jest/globals';
 import React from "react";
 import renderer from "react-test-renderer";
-import {mdc} from "../../Mod20/MDC"
+import {desafio-1} from "../../Mod20/MDC"
 
 
 test('This is validated', () => {
-    const tree = renderer.create(<mdc />).toJSON();
+    const tree = renderer.create(<desafio-1 />).toJSON();
     expect(tree).toMatchSnapshot();
 });
 
 test('That result is equal', () => {
-    expect(mdc(10,20)).toEqual(10);
+    expect(desafio-1(10,20)).toEqual(10);
 })
